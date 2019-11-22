f893963309cf177f0ecaf07c70b1d65644ded305
Guðmundur Snorri Sigfússon <gummehh@gmail.com>
2019-11-22 02:22:19 +0000

    Update GithubTodos.js

--- client/GithubTodos.js
+++ client/GithubTodos.js
@@ -1,5 +1,12 @@
-
+/*
   Todo: 
-  Setja mín context í eitt stórt context.
+  Eyða ShowBurgerMenu. Senda niður með prop 
   Breyta Textum í Context í stað Json.
   
+  Language Context : https://medium.com/better-programming/react-context-hooks-part-2-ui-language-switch-f4610a21379b
+  
+  Skoða:
+  
+  https://tympanus.net/codrops/category/tutorials/
+  
+*/
